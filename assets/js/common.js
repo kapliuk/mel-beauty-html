925b488a9a28d0526cbebc14ed5b4b113285cfb6
Kapliuk <git@github.com:Kapliuk/mel-beauty-html.git>
2026-04-01 17:45:27 +0300

    Start

---
+++ assets/js/common.js
@@ -0,0 +1,110 @@
+/*scroll anchors*/
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+		const target = document.querySelector(this.getAttribute('href'));
+		const offset = 80;
+		const bodyRect = document.body.getBoundingClientRect().top;
+		const elementRect = target.getBoundingClientRect().top;
+		const elementPosition = elementRect - bodyRect;
+		const offsetPosition = elementPosition - offset
+		window.scrollTo({
+			top: offsetPosition,
+			behavior: 'smooth'
+		});
+	});
+})
+
+/*mobile menu*/
+if(document.querySelector('.nav-toggle')){
+	document.querySelector('.nav-toggle').addEventListener('click', function () {
+		document.querySelector('.nav').classList.toggle('nav--open');
+		this.classList.toggle('nav-toggle--active');
+	});
+}
+const links = document.querySelectorAll('.nav__link');
+links.forEach(link => {
+	link.addEventListener('click', () => {
+		document.querySelector('.nav').classList.remove('nav--open');
+		document.querySelector('.nav-toggle').classList.remove('nav-toggle--active');
+	});
+});
+
+
+/*swiper*/
+let servicesSwiper = new Swiper(".services__slider", {
+	loop: false,
+	grabCursor: true,
+	autoHeight: false,
+	breakpoints:{
+		320: {
+			slidesPerView: 1,
+		},
+		768:{
+			slidesPerView: 2,
+		},
+		992: {
+			slidesPerView: 3,
+		},
+		1200:{
+			slidesPerView: 5,
+			spaceBetween: 20,
+		}
+	}
+});
+
+let testimonislsSwiper = new Swiper(".testimonials__slider", {
+	loop: false,
+	grabCursor: true,
+	autoHeight: false,
+	dots: true,
+	pagination: {
+		el: ".testimonials__list-pagination",
+		clickable: true,
+	},
+	breakpoints:{
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+			slidesPerGroup: 1,
+		},
+		992: {
+			slidesPerView: 2,
+			spaceBetween: 50,
+			slidesPerGroup: 2,
+		},
+		1200:{
+			slidesPerView: 3,
+			spaceBetween: 80,
+			slidesPerGroup: 3,
+		}
+	}
+});
+
+/*tabs*/
+const tabs = document.querySelectorAll(".tabs__item");
+const contents = document.querySelectorAll(".tabs__content");
+const indicator = document.querySelector(".tabs__indicator");
+
+function moveIndicator(el) {
+	indicator.style.width = el.offsetWidth + "px";
+	indicator.style.left = el.offsetLeft + "px";
+}
+
+moveIndicator(document.querySelector(".tabs__item.active"));
+
+tabs.forEach(tab => {
+	tab.addEventListener("click", () => {
+		tabs.forEach(t => t.classList.remove("active"));
+		tab.classList.add("active");
+	    moveIndicator(tab);
+
+		const id = tab.getAttribute("data-tab");
+		contents.forEach(content => {
+			content.classList.remove("active");
+			if (content.getAttribute("data-content") === id) {
+				content.classList.add("active");
+			}
+		});
+	});
+});
