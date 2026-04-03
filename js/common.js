50c65a0961c86b4968ba9b9bb95d679e42bafb9e
Kapliuk <git@github.com:Kapliuk/mel-beauty-html.git>
2026-04-03 16:52:12 +0300

    add service.html

--- js/common.js
+++ js/common.js
@@ -39,12 +39,15 @@ let servicesSwiper = new Swiper(".services__slider", {
 	breakpoints:{
 		320: {
 			slidesPerView: 1,
+			spaceBetween: 20,
 		},
 		768:{
 			slidesPerView: 2,
+			spaceBetween: 20,
 		},
 		992: {
 			slidesPerView: 3,
+			spaceBetween: 20,
 		},
 		1200:{
 			slidesPerView: 5,
